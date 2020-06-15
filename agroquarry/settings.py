362ef45149e2f20d92fd71fb2e34293144521441
hellstrike12 <hellstrike12@github.com>
2020-06-15 17:38:46 -0300

    teste 3

--- agroquarry/settings.py
+++ agroquarry/settings.py
@@ -121,7 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = './static_files/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static_files/admin/"),
+    os.path.join(BASE_DIR, "static_files/rest_framework/")
+]
 STATIC_ROOT = './static_files/'
 
 # Access-Control-Allow-Origin
