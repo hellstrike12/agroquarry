9ece4fa2f4162b7b921e66c44b9973c5bbbd39bd
hellstrike12 <hellstrike12@github.com>
2020-06-15 07:33:19 -0300

    Commit #6

--- agroquarry/settings.py
+++ agroquarry/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5-7yg1t@3kb%v-bv6_!kkec*6j22n=m4hnn++w5ow9e-=iqa2f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['agroquarry.herokuapp.com']
 
 
 # Application definition
