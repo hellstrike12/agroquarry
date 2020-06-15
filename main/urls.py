8d01b22feed3da45b4397a2552b6bfc9d3d12b24
hellstrike12 <hellstrike12@github.com>
2020-06-14 23:51:53 -0300

    Commit #3

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from main import views
 
 urlpatterns = [
+    path('', views.index),
     path('soja/', views.SojaList.as_view()),
     path('soja/<int:pk>/', views.SojaDetail.as_view()),
     path('milho/', views.MilhoList.as_view()),
--- main/views.py
+++ main/views.py
@@ -1,12 +1,14 @@
 from main.models import Soja, Milho, Cafe
 from main.serializers import SojaSerializer, MilhoSerializer, CafeSerializer
 from django.http import Http404
+from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
 # Create your views here.
-
+def index(request):
+    return render(request, 'index.html')
 
 class SojaList(APIView):
     """
