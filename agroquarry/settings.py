1fa2a957cbe129320ec74ce67c4e33f156ddddc0
hellstrike12 <hellstrike12@github.com>
2020-06-18 12:11:36 -0300

    Add pagination

--- agroquarry/settings.py
+++ agroquarry/settings.py
@@ -126,3 +126,9 @@ STATIC_ROOT = './static_files/'
 
 # Access-Control-Allow-Origin
 CORS_ORIGIN_ALLOW_ALL = True
+
+# REST Framework
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
